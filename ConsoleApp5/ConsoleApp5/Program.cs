3053e29cebc7895009f81a1da80ee0f0789eb9b0
AlexanderSamuel <alexander.samuel@edmsdental.com>
2020-11-19 20:04:01 -0500

    Question 26

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Adult adult = new Adult("Alex", "Samuel");
+            Child child = new Child("Amalia", "Samuel");
+            string[] adultChild =
+                { adult.FirstName,adult.LastName,child.FirstName,child.LastName};
+            for (int i = 0; i < adultChild.Length; i++)
+            {
+                Console.WriteLine(adultChild[i]);
+            }
+
+        }
+    }
+}
+
+
+
+public class Adult
+{ public string FirstName { get; set; }
+    public string LastName { get; set; }
+    
+    public Adult(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+
+    }
+}
+
+
+public class Child
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Child(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+
+    }
+}
